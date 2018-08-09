925bf9a59f67ca496c27c3119b1648cf78ebdd72
chenxinhui <chenxinhui@xlpayment.com>
2018-08-09 16:39:27 +0800

    add gitkeeper

--- src/screenshot.js
+++ src/screenshot.js
@@ -4,7 +4,7 @@ const { screenshot } = require('./config/defaultConfig');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.baidu.com');
+  await page.goto('http://image.baidu.com/');
   await page.screenshot({ path: `${screenshot}/${Date.now()}.png` });
 
   await browser.close();
